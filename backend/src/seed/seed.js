702f9eaf39d5698222a227ed865e653ad2842aa5
SarbjeetRandhawa <sarbjeetrandhawa081@gmail.com>
2025-11-09 00:33:41 +0530

    updated file structure

--- backend/src/seed/seed.js
+++ backend/src/seed/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const Product = require('../models/Products');
-const seedData = require('./seedData');
+const seedData = require('../seedData');
 
 async function seed() {
   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
